a26850906e243395a52695e844c1465823d5d14b
Salma Hanifah <salma.hanifah11@ui.ac.id>
2022-09-02 14:56:57 +0700

    change settings.py

--- projectOne/settings.py
+++ projectOne/settings.py
@@ -10,10 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
+from pathlib import Path
+
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-
-from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
